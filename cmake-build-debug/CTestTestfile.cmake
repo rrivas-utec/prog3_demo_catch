ac0e59d7c72c0d19db85be3ee99cad15dc347c42
rivasr <rivasr@buildone.co>
2024-06-17 15:14:57 -0500

    Update Github Actions

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rudri/CLionProject/test_demo
-# Build directory: C:/Users/rudri/CLionProject/test_demo/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/rudri/CLionProject/test_demo/cmake-build-debug/test_demo-b12d07c_include.cmake")
--- cmake-build-debug/test_demo-b12d07c_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[Hello, World!]==] C:/Users/rudri/CLionProject/test_demo/cmake-build-debug/test_demo.exe [==[Hello\, World!]==]  )
-set_tests_properties( [==[Hello, World!]==] PROPERTIES WORKING_DIRECTORY C:/Users/rudri/CLionProject/test_demo/cmake-build-debug)
-set( test_demo_TESTS [==[Hello, World!]==])
